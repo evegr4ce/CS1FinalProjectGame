ebcf23fee1ffc6e8f5c27a01c5766dca6b6f0e33
evegr4ce <evecunneen@gmail.com>
2025-11-06 22:19:29 -0700

    titlescreen setup

---
+++ src/TitleScreen.java
@@ -0,0 +1,12 @@
+
+public class TitleScreen extends Game {
+	
+	public TitleScreen() {
+		super();
+	}
+	
+	public void initialize() {
+		BackgroundPanel backgroundPanel = new BackgroundPanel("path/to/your/image.jpg");
+		
+	}
+}
