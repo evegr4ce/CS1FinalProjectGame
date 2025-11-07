6086cd635b89a423b9d2e4f001a3deb9bc37fc4a
atila <atila@10.16.12.240>
2025-11-07 10:03:40 -0700

    island screen!

---
+++ src/IceLevel.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+import javax.swing.*;
+
+public class IceLevel extends JPanel{
+
+	public IceLevel() {
+		
+		setBackground(Color.PINK);
+	}
+	
+}
---
+++ src/MushroomLevel.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+import javax.swing.*;
+
+public class MushroomLevel extends JPanel{
+
+	public MushroomLevel() {
+		setBackground(Color.PINK);
+	}
+}
